7aa53c7ecd7bb3387a7419de08f05ed22e5032d6
Kyrylzh <mobilecode555@gmail.com>
2016-01-15 01:01:01 -0500

    homepage update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ declare var $: any;
 })
 export class HomeComponent implements OnInit {
 
-  i = 0;
+  i = 0;j=0;
   constructor() { }
 
   ngOnInit() {
@@ -16,9 +16,13 @@ export class HomeComponent implements OnInit {
     $("#filo").css('transition','top .2s 10ms');
   }
   
+  select_service(param){
+    $(".option").removeClass("selected");
+    $("."+param).addClass("selected");
+  }
   slider(param){
     let ww = $(".slider").css('width');    
-    if(param == 1){
+    if(param == 2){
       this.i--;
     }
     else{
@@ -28,11 +32,31 @@ export class HomeComponent implements OnInit {
     if(this.i<0){
       this.i = 0;
     }
-    else if(this.i>2){
-      this.i = 2;
+    else if(this.i>3){
+      this.i = 3;
     }  
 
-    $(".slider__item").css({'transition':'left 1s ease-in-out','left':(-1)*(this.i)*parseFloat(ww)+'px'});
+    $(".slider__item1").css({'transition':'left 1s ease-in-out','left':(-1)*(this.i)*parseFloat(ww)+'px'});
+    
+  }
+  slider1(param){
+    let ww = $(".slider1").css('width');  
+  
+    if(param == 2){
+      this.j--;
+    }
+    else{
+      this.j++;
+    }
+
+    if(this.j<0){
+      this.j = 0;
+    }
+    else if(this.j>2){
+      this.j = 2;
+    }  
+
+    $(".slider__item2").css({'transition':'left 1s ease-in-out','left':(-1)*(this.j)*parseFloat(ww)+'px'});
     
   }
 
